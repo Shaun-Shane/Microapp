daf2e68aa63fdcf9b7c58659667f292d0a9b2818
Shaun-Shane <1953181@tongji.edu.cn>
2022-07-19 16:56:47 +0800

    update

---
+++ week2/vdom/js/vdom.js
@@ -0,0 +1,33 @@
+function isElementVdom(vdom) {
+  return typeof vdom == 'object' && typeof vdom.type == 'string';
+}
+
+function isTextVdom (vdom) {
+  return typeof vdom == 'string' || typeof vdom == 'number'
+}
+
+const setAttribute = (dom, key, value) => {
+  if (typeof value == 'function' && key.startsWith('on')) {
+    const eventType = key.slice(2).toLowerCase()
+    dom.addEventListener(eventType, value)
+  } else if (key == 'style' && typeof value == 'object') {
+    Object.assign(dom.style, value)
+  } else if (typeof value != 'object' && typeof value != 'function') {
+    dom.setAttribute(key, value)
+  }
+}
+
+const render = (vdom, parent = null) => {
+  const mount = parent ? (el => parent.appendChild(el)) : (el => el)
+  if (isTextVdom(vdom)) {
+    return mount(document.createTextNode(vdom))
+  } else if (isElementVdom(vdom)) {
+    const dom = mount(document.createElement(vdom.type))
+    for (const child of vdom.children) {
+      render(child, dom)
+    }
+    for (const prop in vdom.props) {
+      setAttribute(dom, prop, vdom.props[prop]);
+    }
+  }
+}
