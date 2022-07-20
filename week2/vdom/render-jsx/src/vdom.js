153b5d80441cab223740225e6ee8dafc217e09db
Shaun-Shane <1953181@tongji.edu.cn>
2022-07-20 16:28:34 +0800

    Update vdom.js

--- week2/vdom/render-jsx/src/vdom.js
+++ week2/vdom/render-jsx/src/vdom.js
@@ -41,10 +41,14 @@ const render = (vdom, parent = null) => {
   }
 }
 
+const data = {
+  item1: 'bbb',
+  item2: 'ddd'
+}
 const jsx = <ul className="list">
   <li className="item" style={{ background: 'blue', color: 'pink' }} onClick={() => alert(2)}>aaa</li>
-  <li className="item">bbbb</li>
-  <li className="item">cccc</li>
+  <li className="item">{data.item1}</li>
+  <li className="item">{data.item2}</li>
 </ul>
 
 render(jsx, document.getElementById('app'))
